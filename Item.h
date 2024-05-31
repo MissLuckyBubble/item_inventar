df6c8506ae0bd71e1322708ed4c955d0798e105c
MissLuckyBubble <qvkatadlg9@gmail.com>
2024-05-31 09:40:31 +0300

    first commit

---
+++ Item.h
@@ -0,0 +1,28 @@
+
+#include <string>
+
+class Item {
+private:
+    std::string name;
+    int id;
+    int quantity;
+    double price;
+    std::string supplier;
+
+public:
+    Item(std::string name, int id, int quantity, double price, std::string supplier);
+
+    std::string getName() const;
+    int getId() const;
+    int getQuantity() const;
+    double getPrice() const;
+    std::string getSupplier() const;
+
+    void setName(const std::string& newName);
+    void setQuantity(int newQuantity);
+    void setPrice(double newPrice);
+    void setSupplier(const std::string& newSupplier);
+
+    void displayItem() const;
+
+};
